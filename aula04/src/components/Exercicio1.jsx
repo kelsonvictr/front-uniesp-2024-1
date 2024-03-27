dee58c00202868537574d7c4004b0574b445904c
Kelson Ameida <kelson.victr@gmail.com>
2024-03-27 19:48:57 -0300

    Aula 04 - andamento

---
+++ aula04/src/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <p>Teste de CSS</p>
+        <img src='https://img.elo7.com.br/product/original/3222EB9/bob-esponja-e-patrick-em-camadas-arquivo-de-corte-bob-esponja.jpg' />
+    </div>
+  )
+}
+
+export default Exercicio1
