2ac33e7bf5f69165a432bbf4e24f0ea6b7913c29
Kelson Ameida <kelson.victr@gmail.com>
2024-03-27 20:47:03 -0300

    Aula 04

--- aula04/src/App.jsx
+++ aula04/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Exercicio1 from './components/Exercicio1'
+import Exercicio2 from './components/Exercicio2'
+import Campanha from './components/Campanha'
 
 const App = () => {
   return (
     <div>
-      <Exercicio1 />
+      <Campanha mes="novembro" />
     </div>
   )
 }
