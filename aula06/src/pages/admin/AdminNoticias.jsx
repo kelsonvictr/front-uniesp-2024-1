20061d2cbe8dea8d4745abc1eb4fc2d760a8e140
Kelson Ameida <kelson.victr@gmail.com>
2024-05-22 19:50:22 -0300

    ...

--- aula06/src/pages/admin/AdminNoticias.jsx
+++ aula06/src/pages/admin/AdminNoticias.jsx
@@ -21,10 +21,50 @@ const AdminNoticias = () => {
         fetchData()
       }, [])
 
+      const removerNoticia = async (id) => {
+        try {
+            await axios.delete(`${url}/${id}`)
+            setNoticias(noticias.filter(noticia => noticia.id !== id))
+        } catch (error) {
+            console.error("Erro ao remover a notícia ", error)
+        }
+      }
+
 
 
   return (
-    <div>AdminNoticias</div>
+    <div>
+      <Typography variant="h4" gutterBottom>Gerenciar Notícias</Typography>
+      <Button variant="contained" color="primary" component={Link} to="/cadastrar-noticia">
+        Cadastrar Nova Notícia
+      </Button>
+      <List>
+        {noticias.map((noticia) => (
+          <ListItem key={noticia.id} divider>
+            <ListItemText
+              primary={noticia.titulo}
+              secondary={noticia.subtitulo}
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to={`/editar-noticia/${noticia.id}`}
+            >
+              Editar
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => removerNoticia(noticia.id)}
+              style={{ marginLeft: '10px' }}
+            >
+              Remover
+            </Button>
+          </ListItem>
+        ))}
+      </List>
+    </div>
   )
 }
 
